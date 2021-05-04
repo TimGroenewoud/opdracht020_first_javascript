1282b3a8eeaffbfa58571b798ec951cb01d4367e
Tim Groenewoud <74865860+TimGroenewoud@users.noreply.github.com>
2021-05-04 20:32:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("Hello Winc Academy")
+
+let firstName = 'Tim';
+console.log(firstName);
+
+let somNumbers = 4 + 4;
+console.log(somNumbers);
+
+let somStrings = "4" + "4";
+console.log(somStrings);
+
+let somAddition = 10 + 10;
+console.log(somAddition)
+
+let somSubtraction = 100 - 50;
+console.log(somSubtraction)
+
+let somMultiplication = 22 * 22;
+console.log(somMultiplication)
+
+let somDivision = 30 / 2;
+console.log(somDivision)
+
+let somModulus = 5 % 30;
+console.log(somModulus)
+
+let myAge = 'vijf en dertig';
+console.log(myAge);
+
+
